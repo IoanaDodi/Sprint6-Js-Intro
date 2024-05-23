6f1f325d335f448d7862368dca74c65bee3cd4d3
IoanaDodi <ioanadodita1998@gmail.com>
2024-05-23 17:43:37 +0300

    tema 2 gata

---
+++ ex2/index.js
@@ -0,0 +1,8 @@
+function tellFortune(N, Z, Y, X) {
+    
+    console.log("Voi fi o " + X + " in " + Y +" casatorita cu " + Z + " si vom avea " + N + " copii");
+   
+}
+
+tellFortune(2, "Vincentiu", "Elvetia", "programatoare");
+
